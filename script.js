ba3a2ef354a268b4de2d74f6b5e8bbeeb8ef77e7
Tạ Hoàng Phi Hùng <konan111223@gmail.com>
2024-07-11 05:06:03 +0700

    f

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function copyText(elementId) {
     const inputElement = document.getElementById(elementId);
     inputElement.select();
-    inputElement.setSelectionRange(0, 99999); // For mobile devices
-
+    inputElement.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(inputElement.value).then(() => {
-        alert('Copied to clipboard');
+        showNotification('Copied to clipboard');
     }).catch(err => {
         console.error('Failed to copy: ', err);
     });
+}
+
+function showNotification(message) {
+    const notification = document.getElementById('notification');
+    notification.textContent = message;
+    notification.style.display = 'block';
+    setTimeout(() => {
+        notification.style.display = 'none';
+    }, 2000);
 }
